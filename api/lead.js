0481fc8eb4e30f66deb881d751122d9f0d788b0f
Stephen Lovino <stephen.ben19@gmail.com>
2025-10-07 01:37:44 +0800

    Remove Node.js runtime export from lead API file to streamline configuration.

--- api/lead.js
+++ api/lead.js
@@ -1,4 +1,3 @@
-export const config = { runtime: 'nodejs18.x' };
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
